d8a5edc6a95c3ebd3db6d5018f9b453cda9024b0
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-07-19 14:31:09 +0800

    Add and initialize controllers

---
+++ BadmintonEventsWebApp/Controllers/TournamentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BadmintonEventsWebApp.Controllers
+{
+    public class TournamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
