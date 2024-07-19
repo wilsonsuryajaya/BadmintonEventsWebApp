9456a99e5547a8ec182c2e37b700b41feecd0a04
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-07-19 12:57:50 +0800

    Initial Commit

---
+++ BadmintonEventsWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BadmintonEventsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+    }
+}
