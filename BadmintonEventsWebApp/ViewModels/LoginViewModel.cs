53b257b1738979305afd6944cbdb4e2b7d656f94
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-07-22 17:51:37 +0800

    Added Login Page

---
+++ BadmintonEventsWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BadmintonEventsWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required()]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
